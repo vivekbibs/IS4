86840d767eb831efa55c62b831909f6118f3c4cf
Vivek Ashokan <vivek.ashokan@outlook.fr>
2025-01-14 16:30:32 +0100

    correction project_id

--- test_cluster/exemple_script.sh
+++ test_cluster/exemple_script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-
+# https://ifb-elixirfr.gitlab.io/cluster/doc/quick-start/#1-write-a-bash-script
 #SBATCH --job-name=bowtie
 #SBATCH --mem=40GB
-#SBATCH --account=yourproject
+
 
 bowtie2 -x hg19 -1 sample_R1.fq.gz -2 sample_R2.fq.gz -S sample_hg19.sam
 
