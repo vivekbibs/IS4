6fc8098bfab318c39bfe08ef0757a6c2e5b15e85
Vivek ASHOKAN <vashokan@clust-slurm-client2.ifb.local>
2025-01-31 18:05:48 +0100

    correction meteor

--- meteor/launch_meteor_mapping.sh
+++ meteor/launch_meteor_mapping.sh
@@ -8,9 +8,9 @@ source /shared/projects/mudis4ls_is4_benchmark/miniforge3/bin/activate /shared/p
 
 # Définir les variables
 meteor_bd_path="/shared/projects/mudis4ls_is4_benchmark/BDs/hs_10_4_gut"
-fastq_dir="/shared/projects/mudis4ls_is4_benchmark/test_data_meteor/fastq/"
-fastq_file="ERS12377136.fastq.gz"
-name="${fastq_file%.fastq.gz}"
+# fastq_dir="/shared/projects/mudis4ls_is4_benchmark/test_data_meteor/fastq/"
+# fastq_file="ERS12377136.fastq.gz"
+# name="${fastq_file%.fastq.gz}"
 
 fastq_file_path="${fastq_dir}${fastq_file}"
 
@@ -22,8 +22,8 @@ mkdir -p $output_dir
 
 
 
-meteor fastq -i /shared/projects/mudis4ls_is4_benchmark/test_data_meteor/only_one/fastq \
--o /shared/projects/mudis4ls_is4_benchmark/test_data_meteor/only_one/sample/
+meteor fastq -i "$fastq_dir" \
+-o "$dir/sample/"
 
 meteor mapping -i $fastq_dir \
 -r $meteor_bd_path \
