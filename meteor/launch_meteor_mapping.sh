54ce39d0579a470e3713af10f1d19496985d429d
Vivek ASHOKAN <vashokan@clust-slurm-client2.ifb.local>
2025-01-31 17:46:40 +0100

    correction meteor

--- meteor/launch_meteor_mapping.sh
+++ meteor/launch_meteor_mapping.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=meteor_mapping
+#SBATCH --job-name=meteor_fastq_mapping
 #SBATCH --mem=150GB
 #SBATCH --cpus-per-task=8
 
@@ -14,13 +14,18 @@ name="${fastq_file%.fastq.gz}"
 
 fastq_file_path="${fastq_dir}${fastq_file}"
 
+dir="/shared/projects/mudis4ls_is4_benchmark/test_data_meteor/only_one"
+fastq_dir="$dir/fastq"
 # Définir le répertoire de sortie
 output_dir="/shared/projects/mudis4ls_is4_benchmark/test_results/meteor/mapping"
 mkdir -p $output_dir
 
 
 
-meteor mapping -i $fastq_file_path \
+meteor fastq -i /shared/projects/mudis4ls_is4_benchmark/test_data_meteor/only_one/fastq \
+-o /shared/projects/mudis4ls_is4_benchmark/test_data_meteor/only_one/sample/
+
+meteor mapping -i $fastq_dir\
 -r $meteor_bd_path \
 -o $output_dir \
 -t $SLURM_CPUS_PER_TASK
