ef92dda0d6c57debea4116cbd76fbe7e3a81a942
Vivek ASHOKAN <vashokan@clust-slurm-client2.ifb.local>
2025-02-05 10:43:24 +0100

    update bracken -x flag

--- kraken/bracken.sh
+++ kraken/bracken.sh
@@ -2,12 +2,12 @@
 # https://ifb-elixirfr.gitlab.io/cluster/doc/quick-start/#1-write-a-bash-script
 #SBATCH --job-name=bracken_classify
 #SBATCH --mem=120GB
-#SBATCH --cpus-per-task=20
+#SBATCH --cpus-per-task=15
 # Activer l'environnement Conda 
 source /shared/projects/mudis4ls_is4_benchmark/miniforge3/bin/activate /shared/projects/mudis4ls_is4_benchmark/conda/envs/bracken
 
 
 DB_DIR="/shared/projects/mudis4ls_is4_benchmark/BDs"
-KRAKEN_INSTALLATION="/shared/projects/mudis4ls_is4_benchmark/conda/pkgs/kraken2-2.1.3-pl5321h077b44d_4/bin/kraken2"
+KRAKEN_INSTALLATION="/shared/projects/mudis4ls_is4_benchmark/conda/pkgs/kraken2-2.1.3-pl5321h077b44d_4/share/kraken2-2.1.3-4"
 # k=35 pour kraken2, default kmer length
-bracken-build -d $DB_DIR -t $SLURM_CPUS_PER_TASK -k ${KMER_LEN} -l ${READ_LEN} -x $KRAKEN_INSTALLATION
+bracken-build -x $KRAKEN_INSTALLATION -d $DB_DIR -t $SLURM_CPUS_PER_TASK -k ${KMER_LEN} -l ${READ_LEN} 
